9380556f87856f2e7bb5d690897d1c0f0038e7fd
luckysher <luckyshera95@gmail.com>
2017-12-25 23:29:45 +0530

    Added imports for rest controller

--- src/main/java/blogger/rest/controller/BloggerRestController.java
+++ src/main/java/blogger/rest/controller/BloggerRestController.java
@@ -7,3 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import blogger.rest.BloggerServiceImpl.BloggerServiceImpl;
+
+
