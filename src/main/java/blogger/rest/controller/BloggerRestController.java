d08b7c8bc60c7f607b19cd269172c139a43eb723
luckysher <luckyshera95@gmail.com>
2017-12-25 23:27:45 +0530

    added file for rest controller

--- src/main/java/blogger/rest/controller/BloggerRestController.java
+++ src/main/java/blogger/rest/controller/BloggerRestController.java
@@ -1,4 +1,9 @@
 package blogger.rest.controller;
 
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
