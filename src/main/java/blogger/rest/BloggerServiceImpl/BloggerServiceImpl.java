846d58129efc90b3c6ed24a52020c6bbc7ed4cf8
luckysher <luckyshera95@gmail.com>
2017-12-25 23:36:47 +0530

    added class for blogger interface impl

---
+++ src/main/java/blogger/rest/BloggerServiceImpl/BloggerServiceImpl.java
@@ -0,0 +1,10 @@
+package blogger.rest.BloggerServiceImpl;
+
+import blogger.rest.BloggerService.BookService;
+
+import com.choudhury.domain.Book;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
