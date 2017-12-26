f612679cfe622654f70d5ad525735a883235a7b1
luckysher <luckyshera95@gmail.com>
2017-12-26 21:39:52 +0530

    added class for adding blog

--- src/main/java/blogger/rest/BloggerService/BloggerServiceImpl.java
+++ src/main/java/blogger/rest/BloggerService/BloggerServiceImpl.java
@@ -7,5 +7,18 @@ import org.slf4j.LoggerFactory;
 import org.springframework.orm.hibernate3.HibernateTemplate;
 import org.springframework.stereotype.Component;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+
+@Component
+// Class for implementing Blogger service 
+public class BloggerServiceImpl implements BloggerService{
+
+  
+}
 
 
