72230dc309e7450cdebaef923b205117e23ca137
luckysher <luckyshera95@gmail.com>
2017-12-26 21:38:47 +0530

    moved to blogger service package

---
+++ src/main/java/blogger/rest/BloggerService/BloggerServiceImpl.java
@@ -0,0 +1,11 @@
+package blogger.rest.BloggerService;
+
+import blogger.rest.models.Blog;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.orm.hibernate3.HibernateTemplate;
+import org.springframework.stereotype.Component;
+
+
+
