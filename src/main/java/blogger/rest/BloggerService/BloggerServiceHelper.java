f8c1c9303c460e51d15237eaff432c45cb03f4da
luckysher <luckyshera95@gmail.com>
2017-12-27 23:42:54 +0530

    added blogger service helper

---
+++ src/main/java/blogger/rest/BloggerService/BloggerServiceHelper.java
@@ -0,0 +1,11 @@
+package blogger.rest.BloggerService;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import blogger.rest.BloggerService.*;
+
+public class BloggerServiceHelper {
+
+}
