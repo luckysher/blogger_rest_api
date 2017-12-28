75168799d9901ba425999bfa170724d08e105bfe
luckysher <luckyshera95@gmail.com>
2017-12-29 00:47:44 +0530

    updated import of controller

--- src/main/java/blogger/rest/controller/BloggerRestController.java
+++ src/main/java/blogger/rest/controller/BloggerRestController.java
@@ -5,14 +5,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.orm.hibernate3.HibernateTemplate;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import blogger.rest.BloggerServiceImpl.BloggerServiceImpl;
 
+import blogger.rest.BloggerService.BloggerServiceHelper;
+import blogger.rest.BloggerService.BloggerServiceImpl;
+import blogger.rest.models.*;
 
 @RestController
 @RequestMapping("/blog")
