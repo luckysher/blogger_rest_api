c2ef5c22cbc777eac92b50e12ff1beacbd0f7d7d
luckysher <luckyshera95@gmail.com>
2017-12-29 00:53:28 +0530

    added imports for model

---
+++ src/main/java/blogger/rest/models/Blog.java
@@ -0,0 +1,8 @@
+package blogger.rest.models;
+
+import java.util.*;
+
+import javax.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.NotBlank;
+import blogger.rest.validator.*;
+
