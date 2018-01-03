f45c031ae5d8cea3a246f18a71bbbfe8c4e41af7
luckysher <luckyshera95@gmail.com>
2018-01-03 23:04:56 +0530

    updated class for validator

--- src/main/java/blogger/rest/validator/DateFormat.java
+++ src/main/java/blogger/rest/validator/DateFormat.java
@@ -2,5 +2,8 @@ package blogger.rest.validator;
 
 
 public @interface DateFormat {
-	
+	String messsage() default "{DateFormat}";
+	Class <?>[] groups() default {};
+	Class <? extends Payload>[] payload() default {};
+
 }
