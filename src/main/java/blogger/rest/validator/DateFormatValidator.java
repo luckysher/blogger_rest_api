cab8b9bd9ef4e9ccd4fe011d4bea21bf4536ae72
luckysher <luckyshera95@gmail.com>
2018-01-04 21:24:52 +0530

    added import for validator class

--- src/main/java/blogger/rest/validator/DateFormatValidator.java
+++ src/main/java/blogger/rest/validator/DateFormatValidator.java
@@ -1,5 +1,8 @@
 package blogger.rest.validator;
 
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
 
 public class DateFormatValidator implements ConstraintValidator<DateFormat, String>{
 
