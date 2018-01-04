d04d5633dd9945a7b04ececbd12f6d7c6af0b577
luckysher <luckyshera95@gmail.com>
2018-01-04 21:30:28 +0530

    added method for overidding

--- src/main/java/blogger/rest/validator/DateFormatValidator.java
+++ src/main/java/blogger/rest/validator/DateFormatValidator.java
@@ -6,4 +6,11 @@ import javax.validation.ConstraintValidatorContext;
 
 public class DateFormatValidator implements ConstraintValidator<DateFormat, String>{
 
+	@Override
+	public void initialize(DateFormat date) {
+		// TODO Auto-generated method stub		
+	}
+
+	
+
 }
