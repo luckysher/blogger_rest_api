241f379482037c01a94e8739291a8458a0188b7b
luckysher <luckyshera95@gmail.com>
2018-01-06 20:35:51 +0530

    added class for making request

---
+++ src/main/java/blogger/rest/test/java/blogger/rest/controllers/HttpRequest.java
@@ -0,0 +1,110 @@
+package blogger.rest.controllers;
+
+
+//**********************************************//
+// Class for making http requests for testing   //
+//**********************************************//
+
+import blogger.rest.models.*;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+import org.json.simple.*;
+
+public class HttpRequest {
+        
+	 public URL url;
+	 public HttpURLConnection con;	 
+	 public String accept="application/json";
+	 public String userAgent = "Mozila/5.0";
+	 public String contentType="application/json";
+	 
+	 public void setUrl(String url) throws MalformedURLException{
+		   this.url = new URL(url);		 
+	     }
+	 
+	 public void setContentType(String contentType){
+		 this.contentType = contentType;
+	 }
+	
+	 public void setAcceptType(String acceptType){
+		 this.accept = acceptType;
+	 }
+	 
+	 public boolean connect(){		 
+		 boolean connected = false;
+		 try{
+			 this.con = (HttpURLConnection)this.url.openConnection();
+			 this.con.addRequestProperty("Accept", this.accept);
+		     this.con.addRequestProperty("User-Agent", this.userAgent);		     
+		     connected = true;
+		 }catch(Exception e){			 
+			 System.out.println("Got exception while connecting to server");
+			 System.out.println("Exception : " + e);
+		 }
+		 return connected;
+	}
+	 
+	 public void setRequestMethod(String method){
+		 boolean connected = this.connect();
+		 if (connected){
+		 List<String> methods = Arrays.asList("GET", "POST");
+		 if(methods.contains(method)){
+			    if (method.equals("POST")){ 
+			    	//this.setContentType("application/json");
+			    	System.out.println("Set content type: a/json ");
+			    	this.con.addRequestProperty("Method", method);
+			    	this.con.addRequestProperty("Accept", "application/json");
+			    	this.con.addRequestProperty("Content-Type", "application/json");
+			    }else{
+			    	this.con.addRequestProperty("Method", method);
+			    }
+		 }else{
+			 	System.out.println("Unable to set the requested method : '" + method + "'");
+		 	}
+		  }
+		 }
+	 
+	 public String doGetRequest() throws IOException{		
+		 StringBuffer response = null;		 
+		 BufferedReader br = new BufferedReader(new InputStreamReader(this.con.getInputStream()));
+		 response = new StringBuffer();		 
+		 String readline=null;
+		 if (this.con.getResponseCode() == HttpURLConnection.HTTP_OK){
+			 while((readline = br.readLine()) != null){
+				 response.append(readline);			 
+		 	}    
+		 }
+		 return response.toString();
+	 }
+	 
+	 public String doPostRequest(String blog) throws IOException{		 
+		 StringBuffer response = null;		 
+		 System.out.println("Doing post request: ");
+		 this.con.setDoOutput(true);
+		 OutputStreamWriter osw = new OutputStreamWriter(con.getOutputStream());
+		 osw.write(blog);
+		 osw.flush();
+		 osw.close();
+		 response = new StringBuffer();	
+		 response.append("added blog successfully : " + this.con.getResponseCode());
+		 BufferedReader br = new BufferedReader(new InputStreamReader(this.con.getInputStream()));
+		response = new StringBuffer();		 
+		String readline=null;
+		 if (this.con.getResponseCode() == HttpURLConnection.HTTP_OK){
+			 while((readline = br.readLine()) != null){
+				 response.append(readline);			 
+		 	}    
+		 }
+		 return response.toString();
+	 }
+}
