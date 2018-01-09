a18ba9e2712e149cff1efab3df7fc84a17677c41
luckysher <luckyshera95@gmail.com>
2018-01-09 19:59:45 +0530

    added file for doing request call for testing urls

---
+++ src/main/java/blogger/rest/test/java/blogger/rest/controllers/HttpRequest.java
@@ -0,0 +1,15 @@
+package blogger.rest.controllers;
+
+
+//**********************************************//
+// Class for making http requests for testing   //
+//**********************************************//
+
+import blogger.rest.models.*;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+
